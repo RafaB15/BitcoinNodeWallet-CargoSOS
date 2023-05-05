b54c6f4f5cba8b1b4e3b382d413ce2dbcccc9ca7
Rafael <rafaelberenguel15@gmail.com>
2023-05-05 16:48:29 -0300

    Función para que node genere version message

--- src/node_structure/node.rs
+++ src/node_structure/node.rs
@@ -8,6 +8,7 @@ use std::net::{
     Ipv6Addr,
     SocketAddr
 };
+use crate::connections::socket_conversion::socket_to_ipv6_port;
 use crate::messages::version_message::VersionMessage;
 use chrono::offset::Utc;
 
@@ -42,20 +43,20 @@ impl Node {
         }
     }
 
-
     ///Function that tries to build a version message with the current information of the node
     pub fn build_version_message(
         &self,
-        recv_addr: Ipv6Addr,
+        recv_socket_addr: SocketAddr,
         recv_services: SupportedServices,
-        trans_addr: Ipv6Addr,
-        port: u16,
+        trans_socket_addr: SocketAddr,
         nonce: u64,
         user_agent: String,
         relay: bool
     ) ->  VersionMessage {
 
         let timestamp = Utc::now();
+        let (recv_addr, recv_port) = socket_to_ipv6_port(&recv_socket_addr);
+        let (trans_addr, trans_port) = socket_to_ipv6_port(&trans_socket_addr);
         
         VersionMessage::new(
             self.protocol_version, 
@@ -63,14 +64,17 @@ impl Node {
             timestamp, 
             recv_services, 
             recv_addr, 
-            port, 
+            recv_port, 
             trans_addr, 
-            port, 
+            trans_port, 
             nonce, 
             user_agent, 
             self.blockchain_height, 
             relay)
     }
+
+    
+
     /*
     ///Función que intenta hacer el handshake
     pub fn handshake(potential_peers: Vec<SocketAddr>) {
