cbdee17ee48c24127c6aed2aecda480bc9fb3d17
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-06 16:29:54 -0300

    Empezando con handshake y mandar mensajes de version

--- src/node_structure/node.rs
+++ src/node_structure/node.rs
@@ -1,12 +1,17 @@
 use crate::connections::{
     p2p_protocol::ProtocolVersionP2P,
     ibd_methods::IBDMethod,
-    suppored_services::SupportedServices
+    suppored_services::SupportedServices,
+    connection_error::ConnectionError
 };
+use crate::messages::message;
+use crate::messages::serializable::Serializable;
 
 use std::net::{
     Ipv6Addr,
-    SocketAddr
+    SocketAddr,
+    TcpStream,
+    TcpListener
 };
 use crate::connections::socket_conversion::socket_to_ipv6_port;
 
@@ -14,6 +19,7 @@ use crate::messages::{
     message::Message,
     version_message::VersionMessage,
     payload::Payload,
+    error_message::ErrorMessage,
 };
 
 use chrono::offset::Utc;
@@ -21,7 +27,8 @@ use chrono::offset::Utc;
 const IGNORE_NONCE: u64 = 0;
 const IGNORE_USER_AGENT: &str = "";
 const NO_NEW_TRANSACTIONS: bool = false; 
-
+const NO_RELAY: bool = false;
+const TESTNET_MAGIC_NUMBERS: [u8; 4] = [0x0b, 0x11, 0x09, 0x07];
 
 pub struct Node {
     protocol_version: ProtocolVersionP2P,
@@ -52,17 +59,17 @@ impl Node {
     ///Function that builds a version message payload with the current information of the node
     pub fn build_version_message_payload(
         &self,
-        recv_socket_addr: SocketAddr,
+        recv_socket_addr: &SocketAddr,
         recv_services: SupportedServices,
-        trans_socket_addr: SocketAddr,
+        trans_socket_addr: &SocketAddr,
         nonce: u64,
         user_agent: String,
         relay: bool
     ) ->  Payload {
 
         let timestamp = Utc::now();
-        let (recv_addr, recv_port) = socket_to_ipv6_port(&recv_socket_addr);
-        let (trans_addr, trans_port) = socket_to_ipv6_port(&trans_socket_addr);
+        let (recv_addr, recv_port) = socket_to_ipv6_port(recv_socket_addr);
+        let (trans_addr, trans_port) = socket_to_ipv6_port(trans_socket_addr);
         
         let payload = VersionMessage::new(
             self.protocol_version, 
@@ -81,12 +88,13 @@ impl Node {
         Payload::VersionMessage(payload)
     }
 
+    ///Function that builds a version message with the current information of the node
     pub fn build_version_message(
         &self,
         magic_bytes: [u8; 4],
-        recv_socket_addr: SocketAddr,
+        recv_socket_addr: &SocketAddr,
         recv_services: SupportedServices,
-        trans_socket_addr: SocketAddr,
+        trans_socket_addr: &SocketAddr,
         nonce: u64,
         user_agent: String,
         relay: bool
@@ -96,11 +104,55 @@ impl Node {
         Message::new(magic_bytes, payload)
     }
 
-    /*
-    ///Función que intenta hacer el handshake
-    pub fn handshake(potential_peers: Vec<SocketAddr>) {
-
+    pub fn send_testnet_version_message(&self, local_socket_addr: &SocketAddr, potential_peer: &SocketAddr, potencial_peer_stream: &mut TcpStream) -> Result<(), ErrorMessage>{
+        let version_message = self.build_version_message(
+            TESTNET_MAGIC_NUMBERS, 
+            potential_peer, 
+            SupportedServices::Unname, 
+            local_socket_addr, 
+            IGNORE_NONCE, 
+            IGNORE_USER_AGENT.to_string(), 
+            NO_NEW_TRANSACTIONS);
+            
+        version_message.serialize(potencial_peer_stream)?;
+        Ok(())
     }
-    */
+
+    ///Function that tries to do the handshake with the given potential peer.
+    pub fn attempt_connection_with_testnet_peer(&self, potential_peer: &SocketAddr) -> Result<(), ConnectionError>{
+
+        let mut potencial_peer_stream = match TcpStream::connect(potential_peer) {
+            Ok(stream) => stream,
+            Err(_) => return Err(ConnectionError::ErrorCannotConnectToAddress),
+        };
+
+        let local_socket_addr = match potencial_peer_stream.local_addr() {
+            Ok(addr) => addr,
+            Err(_) => return Err(ConnectionError::ErrorCannotObtainOwnAddress),
+        };
+
+        match self.send_testnet_version_message(&local_socket_addr, potential_peer, &mut potencial_peer_stream) {
+            Ok(_) => println!("Version message sent to peer {}", potential_peer),
+            Err(e) => {
+                println!("Error while sending version message to peer {}: {:?}", potential_peer, e);
+                return Err(ConnectionError::ErrorCannotSendMessage);
+            }
+        };
+
+        Ok(())
+    }
+
+
+    ///Function that tries to do the handshake with the given vector of potential peers.
+    pub fn connect_to_testnet_peers(&self, potential_peers: &Vec<SocketAddr>) -> Result<(), ConnectionError> {
+        for potential_peer in potential_peers {
+            match self.attempt_connection_with_testnet_peer(potential_peer) {
+                Ok(_) => println!("Connection with peer {} established", potential_peer),
+                Err(e) => println!("Error while trying to connect to peer {}: {:?}", potential_peer, e)
+            }
+        }
+        Ok(())
+    }
+
 
 }
