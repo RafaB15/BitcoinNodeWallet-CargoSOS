58a7f1bf3f1b73d194b13c5146dbc2a76c783992
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-08 00:45:30 -0300

    Agregando el tipo hash al strcut

--- src/messages/get_headers_message.rs
+++ src/messages/get_headers_message.rs
@@ -4,9 +4,16 @@ use super::{
     error_message::ErrorMessage,
 };
 
+use bitcoin_hashes::sha256d::Hash as Sha256dHash;
+
+use crate::connections::p2p_protocol::ProtocolVersionP2P;
+
+pub const GET_HEADERS_TYPE: [u8; 12] = *b"getheaders\0\0";
+
 pub struct GetHeadersMessage {
-    pub version: u32,
-    pub hash_count: u8,
-    pub block_locator_hashes: Vec<[u8; 32]>,
-    pub hash_stop: [u8; 32],
+    pub version: ProtocolVersionP2P,
+    pub block_locator_hashes: Vec<Sha256dHash>, //Al serializar esto hay que primero poner el compact size u y luego los hashes
+    pub hash_stop: Sha256dHash,
 }
+
+
