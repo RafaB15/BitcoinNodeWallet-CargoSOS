43ec0c41b84ad36526d7ecb4c48b3fccc94d1f23
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-13 23:49:54 -0300

    Creo la función get headers que deberá madnar un get headers message y recibir un headers

--- src/node_structure/initial_block_download.rs
+++ src/node_structure/initial_block_download.rs
@@ -6,11 +6,11 @@ use std::net::{
 use crate::messages::{
     error_message::ErrorMessage,
     get_headers_message::GetHeadersMessage,
+    headers_message::HeadersMessage,
 };
 
 use crate::block_structure::{
     block_chain::BlockChain,
-    block::Block,
     block_header::BlockHeader,
     hash::{
         HashType,
@@ -18,6 +18,7 @@ use crate::block_structure::{
     }
 };
 
+use crate::serialization::deserializable::Deserializable;
 use crate::serialization::serializable::Serializable;
 
 use crate::{
@@ -29,18 +30,16 @@ const NO_STOP_HASH: HashType = [0; 32];
 
 pub struct InitialBlockDownload {
     pub protocol_version: ProtocolVersionP2P,
-    pub peers_adrrs: Vec<SocketAddr>,
 }
 
 impl InitialBlockDownload {
     pub fn new(protocol_version: ProtocolVersionP2P, peers_adrrs: Vec<SocketAddr>) -> Self {
         InitialBlockDownload {
             protocol_version,
-            peers_adrrs,
         }
     }
 
-    pub fn send_get_headers_message(&self, peer_stream: &mut TcpStream, block_chain: BlockChain) -> Result<BlockChain, ErrorMessage>{
+    pub fn send_get_headers_message(&self, peer_stream: &mut TcpStream, block_chain: &BlockChain) -> Result<(), ErrorMessage>{
         let last_header: &BlockHeader = &block_chain.last().header;
         let mut serialized_header = Vec::new();
 
@@ -55,6 +54,18 @@ impl InitialBlockDownload {
             NO_STOP_HASH,
         );
         get_headers_message.serialize(peer_stream)?;
-        Ok(block_chain)
+        Ok(())
+    }
+
+    pub fn add_headers_to_blockchain(&self, block_chain: &mut BlockChain, received_headers_message: &HeadersMessage) -> Result<u32,ErrorMessage> {
+        todo!()
+    }
+
+    pub fn get_headers(&self, peer_stream: &mut TcpStream, block_chain: &mut BlockChain) -> Result<u32,ErrorMessage>{
+        self.send_get_headers_message(peer_stream, block_chain)?;
+        let received_headers_message = HeadersMessage::deserialize(peer_stream)?;
+        let added_headers = self.add_headers_to_blockchain(block_chain, &received_headers_message)?;
+        
+        Ok(added_headers)
     }
 }
