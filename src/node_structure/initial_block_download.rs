16033fb5cfdf82db9399f81a38fd4b97976c9a0f
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-14 12:13:20 -0300

    Se implementa la función para agregar los headers obtenidos y algunas conversiones de errores

--- src/node_structure/initial_block_download.rs
+++ src/node_structure/initial_block_download.rs
@@ -1,5 +1,4 @@
 use std::net::{
-    SocketAddr,
     TcpStream,
 };
 
@@ -18,6 +17,8 @@ use crate::block_structure::{
     }
 };
 
+use super::error_node::ErrorNode;
+
 use crate::serialization::deserializable::Deserializable;
 use crate::serialization::serializable::Serializable;
 
@@ -33,7 +34,7 @@ pub struct InitialBlockDownload {
 }
 
 impl InitialBlockDownload {
-    pub fn new(protocol_version: ProtocolVersionP2P, peers_adrrs: Vec<SocketAddr>) -> Self {
+    pub fn new(protocol_version: ProtocolVersionP2P) -> Self {
         InitialBlockDownload {
             protocol_version,
         }
@@ -57,15 +58,23 @@ impl InitialBlockDownload {
         Ok(())
     }
 
-    pub fn add_headers_to_blockchain(&self, block_chain: &mut BlockChain, received_headers_message: &HeadersMessage) -> Result<u32,ErrorMessage> {
-        todo!()
+    pub fn add_headers_to_blockchain(&self, block_chain: &mut BlockChain, received_headers_message: &HeadersMessage) -> Result<u32,ErrorNode> {
+        let mut added_headers = 0;
+        for header in &received_headers_message.headers {
+            if header.proof_of_work() == false {
+                return Err(ErrorNode::ErrorWhileValidating("Error while validating proof of work".to_string()));
+            }
+            if block_chain.append_header(header.clone()).is_ok() {
+                added_headers += 1;
+            }
+        }
+        Ok(added_headers)
     }
 
-    pub fn get_headers(&self, peer_stream: &mut TcpStream, block_chain: &mut BlockChain) -> Result<u32,ErrorMessage>{
+    pub fn get_headers(&self, peer_stream: &mut TcpStream, block_chain: &mut BlockChain) -> Result<u32,ErrorNode>{
         self.send_get_headers_message(peer_stream, block_chain)?;
         let received_headers_message = HeadersMessage::deserialize(peer_stream)?;
         let added_headers = self.add_headers_to_blockchain(block_chain, &received_headers_message)?;
-        
         Ok(added_headers)
     }
 }
