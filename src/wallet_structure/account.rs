c29c6cca49f80b8b27b91cd250a42bd7de044f18
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-27 17:20:43 -0300

    Mejora en primera versión de la cuenta

--- src/wallet_structure/account.rs
+++ src/wallet_structure/account.rs
@@ -1,5 +1,29 @@
+use super::{
+    error_wallet::ErrorWallet,
+    private_key::PrivateKey,
+    public_key::PublicKey,
+    address::Address,
+};
+
 pub struct Account {
-    pub name: String,
-    pub private_key: String,
-    pub public_key: String,
+    pub account_name: String,
+    pub private_key: PrivateKey,
+    pub public_key: PublicKey,
+    pub address: Address,
+}
+
+impl Account {
+    pub fn new(name: &str, private_key_bytes: &[u8; 32], public_key_bytes: &[u8; 33], addres: &str) -> Result<Account, ErrorWallet> {
+        let account_name = name.to_string();
+        let private_key = PrivateKey::new(private_key_bytes)?;
+        let public_key = PublicKey::new(public_key_bytes)?;
+        let address = Address::new(addres)?;
+
+        Ok(Account {
+            account_name,
+            private_key,
+            public_key,
+            address,
+        })
+    }
 }
