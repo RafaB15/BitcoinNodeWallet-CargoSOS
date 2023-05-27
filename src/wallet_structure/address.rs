78e5b2c0adeb1c23e022e9c0097eea0504f26751
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-27 17:17:09 -0300

    Se implementa una función para extraer la pk hasheada de la address

--- src/wallet_structure/address.rs
+++ src/wallet_structure/address.rs
@@ -8,6 +8,7 @@ pub struct Address {
 }
 
 impl Address {
+    /// Creates an address object from a string with a Bitcoin address
     pub fn new(address: &str) -> Result<Address, ErrorWallet> {
         if address.len() != 34 {
             return Err(ErrorWallet::InvalidAddress(format!("Invalid address length, expected 34, got {}", address.len())));
@@ -25,6 +26,12 @@ impl Address {
             address_string: address.to_string(),
         })
     }
+
+    /// Extracts the hashed public key from the address
+    pub fn extract_hashed_pk(&self) -> &[u8]{
+        let hashed_pk = &self.address_bytes[1..21];
+        hashed_pk
+    }
 }
 
 #[cfg(test)]
@@ -38,5 +45,13 @@ mod tests {
         let address = Address::new(&address).unwrap();
         assert!(address.address_string == "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs");
         assert!(address.address_bytes == address_bytes);
+    }
+
+    #[test]
+    fn test_02_correct_extraction_of_hashed_pk() {
+        let address = "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs".to_string();
+        let hashed_pk: [u8; 20] = [0xf5, 0x4a, 0x58, 0x51, 0xe9, 0x37, 0x2b, 0x87, 0x81, 0x0a, 0x8e, 0x60, 0xcd, 0xd2, 0xe7, 0xcf, 0xd8, 0x0b, 0x6e, 0x31];
+        let address = Address::new(&address).unwrap();
+        assert!(address.extract_hashed_pk() == hashed_pk);
     }
 }
