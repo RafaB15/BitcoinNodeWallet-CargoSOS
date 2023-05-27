969760cb96dd6c4c1ca8df982b9f5491291bbd3f
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-27 15:22:13 -0300

    Se cambia el new

--- src/wallet_structure/private_key.rs
+++ src/wallet_structure/private_key.rs
@@ -1,5 +1,4 @@
 use k256::ecdsa::SigningKey;
-use bs58::decode;
 use std::convert::TryInto;
 
 use super::error_wallet::ErrorWallet;
@@ -13,15 +12,10 @@ impl PrivateKey {
 
     /// Recibe un string que representa una llave privada en formato WIF (no comprimida)
     /// Devuelve un objeto PrivateKey
-    pub fn new(string_key: &str) -> Result<PrivateKey, ErrorWallet> {
-        let decoded_string = match decode(string_key).into_vec() {
-            Ok(decoded_string) => decoded_string,
-            Err(_) => return Err(ErrorWallet::CannotGeneratePrivateKey("Cannot decode private key".to_string())),
-        };
-
-        let key = match SigningKey::from_slice(&decoded_string[1..33]) {
+    pub fn new(private_key_bytes: &[u8; 32]) -> Result<PrivateKey, ErrorWallet> {
+        let key = match SigningKey::from_slice(private_key_bytes) {
             Ok(key) => key,
-            Err(e) => return Err(ErrorWallet::CannotGeneratePrivateKey(format!("Cannot generate SigningKey object from {:?}, error : {:?}",decoded_string, e))),
+            Err(e) => return Err(ErrorWallet::CannotGeneratePrivateKey(format!("Cannot generate SigningKey object from {:?}, error : {:?}", private_key_bytes, e))),
         };
         
         Ok(PrivateKey {
@@ -45,10 +39,9 @@ mod tests {
 
     #[test]
     fn test_01_correct_key_creation() {
-        let private_key_string = "91fTpaUpEJPCgVUJFePpch3XDvP3qs8fVC395DMThYxNBNbusmx".to_string();
-        let private_key = PrivateKey::new(&private_key_string).unwrap();
-        let bytes: [u8; 32] = [0x0a, 0x52, 0x65, 0x08, 0x2e, 0x24, 0x11, 0x5f, 0x77, 0x54, 0x0a, 0xb3, 0xb8, 0xc2, 0xb9, 0x20, 0x60, 0xaa, 0x30, 0xd6, 0xd2, 0xb8, 0x1a, 0x08, 0x5d, 0x71, 0xab, 0x37, 0xed, 0xa7, 0x68, 0x91];
+        let private_key_bytes: [u8; 32] = [0x0a, 0x52, 0x65, 0x08, 0x2e, 0x24, 0x11, 0x5f, 0x77, 0x54, 0x0a, 0xb3, 0xb8, 0xc2, 0xb9, 0x20, 0x60, 0xaa, 0x30, 0xd6, 0xd2, 0xb8, 0x1a, 0x08, 0x5d, 0x71, 0xab, 0x37, 0xed, 0xa7, 0x68, 0x91];
+        let private_key = PrivateKey::new(&private_key_bytes).unwrap();
         let signing_bytes = private_key.as_bytes().unwrap();
-        assert!(signing_bytes == bytes);
+        assert!(signing_bytes == private_key_bytes);
     }
 }
