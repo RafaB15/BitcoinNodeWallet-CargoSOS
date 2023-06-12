1da575dd684fa0058bc04aed96da883ad530be00
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2023-06-12 13:32:07 -0300

    Separando la tui de la gui
    Co-authored-by: Rafael <rafaelberenguel15@gmail.com>
    Co-authored-by: sofiajaves <83650724+sofiajaves@users.noreply.github.com>

--- src/bin/bitcoin/main.rs
+++ src/bin/bitcoin/main.rs
@@ -1,11 +1,12 @@
 mod error_execution;
 mod error_initialization;
 mod process;
+mod tui;
+mod gui;
 
 use std::{
     fs::{File, OpenOptions},
     io::BufReader,
-    net::{SocketAddr, TcpStream},
     path::Path,
     thread::{self, JoinHandle},
 };
@@ -14,22 +15,13 @@ use error_execution::ErrorExecution;
 use error_initialization::ErrorInitialization;
 use process::{
     configuration::Configuration, 
-    download, handshake, account,
-    save_system::SaveSystem, 
     load_system::LoadSystem, 
-};
-
-use cargosos_bitcoin::configurations::{
-    connection_config::ConnectionConfig, download_config::DownloadConfig, log_config::LogConfig,
 };
 
 use cargosos_bitcoin::{
     logs::{error_log::ErrorLog, logger, logger_sender::LoggerSender},
-    block_structure::{
-        block_chain::BlockChain,
-        utxo_set::UTXOSet,
-    },
-    connections::ibd_methods::IBDMethod,
+    configurations::log_config::LogConfig,
+    block_structure::block_chain::BlockChain,
 };
 
 /// Get the configuration name given the arguments
@@ -101,51 +93,9 @@ fn initialize_logs(
     Ok((handle, logger))
 }
 
-fn get_potential_peers(
-    connection_config: ConnectionConfig,
-    logger: LoggerSender,
-) -> Result<Vec<SocketAddr>, ErrorExecution> {
-    logger.log_connection("Getting potential peers with dns seeder".to_string())?;
 
-    let potential_peers = connection_config.dns_seeder.discover_peers()?;
 
-    let peer_count_max = std::cmp::min(connection_config.peer_count_max, potential_peers.len());
 
-    let potential_peers = potential_peers[0..peer_count_max].to_vec();
-
-    for potential_peer in &potential_peers {
-        logger.log_connection(format!("Potential peer: {:?}", potential_peer))?;
-    }
-
-    Ok(potential_peers)
-}
-
-fn get_block_chain(
-    peer_streams: Vec<TcpStream>,
-    block_chain: &mut BlockChain,
-    connection_config: ConnectionConfig,
-    download_config: DownloadConfig,
-    logger: LoggerSender,
-) -> Result<(), ErrorExecution> {
-    logger.log_connection("Getting block chain".to_string())?;
-
-    match connection_config.ibd_method {
-        IBDMethod::HeaderFirst => {
-            download::headers_first(
-                peer_streams,
-                block_chain,
-                connection_config,
-                download_config,
-                logger,
-            )?;
-        }
-        IBDMethod::BlocksFirst => {
-            download::blocks_first();
-        }
-    }
-
-    Ok(())
-}
 
 fn _show_merkle_path(
     block_chain: &BlockChain,
@@ -193,53 +143,6 @@ fn _show_merkle_path(
     Ok(())
 }
 
-fn program_execution(
-    connection_config: ConnectionConfig,
-    download_config: DownloadConfig,
-    load_system: &mut LoadSystem,
-    logger: LoggerSender,
-) -> Result<SaveSystem, ErrorExecution> {
-    let potential_peers = get_potential_peers(connection_config.clone(), logger.clone())?;
-
-    let peer_streams = handshake::connect_to_peers(
-        potential_peers,
-        connection_config.clone(),
-        logger.clone(),
-    );
-
-    let mut block_chain = load_system.get_block_chain()?;
-    let mut wallet = load_system.get_wallet()?;
-
-    println!("Wallet: {:?}", wallet);
-
-    get_block_chain(
-        peer_streams,
-        &mut block_chain,
-        connection_config,
-        download_config,
-        logger.clone(),
-    )?;
-
-    // show_merkle_path(&block_chain, logger_sender.clone())?; 
-
-    while account::wants_to_enter_account()? {
-        let new_account = account::add_account(logger.clone())?;
-        wallet.add_account(new_account);
-    }
-
-    let utxo_set = UTXOSet::from_blockchain(&block_chain);
-
-    for account in wallet.accounts.iter() {
-        print!("Account's {} utxo: {:?}\n", account.account_name, utxo_set.get_utxo_list(&Some(account.address.clone())));
-    }
-
-    Ok(SaveSystem::new(
-        block_chain,
-        wallet,
-        logger,
-    ))
-}
-
 fn main() -> Result<(), ErrorExecution> {
     let arguments: Vec<String> = std::env::args().collect();
 
@@ -260,7 +163,7 @@ fn main() -> Result<(), ErrorExecution> {
             logger.clone(),
         );
     
-        let save_system = program_execution(
+        let save_system = tui::execution::program_execution(
             connection_config,
             download_config,
             &mut load_system,
---
+++ src/bin/bitcoin/tui/execution.rs
@@ -0,0 +1,116 @@
+use crate::{
+    error_execution::ErrorExecution,
+    process::{
+        download, handshake, account,
+        save_system::SaveSystem, 
+        load_system::LoadSystem, 
+    },
+};
+
+use cargosos_bitcoin::configurations::{
+    connection_config::ConnectionConfig, download_config::DownloadConfig
+};
+
+use cargosos_bitcoin::{
+    logs::logger_sender::LoggerSender,
+    block_structure::{
+        block_chain::BlockChain,
+        utxo_set::UTXOSet,
+    },
+    connections::ibd_methods::IBDMethod,
+};
+
+use std::net::{SocketAddr, TcpStream};
+
+fn get_potential_peers(
+    connection_config: ConnectionConfig,
+    logger: LoggerSender,
+) -> Result<Vec<SocketAddr>, ErrorExecution> {
+    logger.log_connection("Getting potential peers with dns seeder".to_string())?;
+
+    let potential_peers = connection_config.dns_seeder.discover_peers()?;
+
+    let peer_count_max = std::cmp::min(connection_config.peer_count_max, potential_peers.len());
+
+    let potential_peers = potential_peers[0..peer_count_max].to_vec();
+
+    for potential_peer in &potential_peers {
+        logger.log_connection(format!("Potential peer: {:?}", potential_peer))?;
+    }
+
+    Ok(potential_peers)
+}
+
+fn get_block_chain(
+    peer_streams: Vec<TcpStream>,
+    block_chain: &mut BlockChain,
+    connection_config: ConnectionConfig,
+    download_config: DownloadConfig,
+    logger: LoggerSender,
+) -> Result<(), ErrorExecution> {
+    logger.log_connection("Getting block chain".to_string())?;
+
+    match connection_config.ibd_method {
+        IBDMethod::HeaderFirst => {
+            download::headers_first(
+                peer_streams,
+                block_chain,
+                connection_config,
+                download_config,
+                logger,
+            )?;
+        }
+        IBDMethod::BlocksFirst => {
+            download::blocks_first();
+        }
+    }
+
+    Ok(())
+}
+
+pub fn program_execution(
+    connection_config: ConnectionConfig,
+    download_config: DownloadConfig,
+    load_system: &mut LoadSystem,
+    logger: LoggerSender,
+) -> Result<SaveSystem, ErrorExecution> {
+    let potential_peers = get_potential_peers(connection_config.clone(), logger.clone())?;
+
+    let peer_streams = handshake::connect_to_peers(
+        potential_peers,
+        connection_config.clone(),
+        logger.clone(),
+    );
+
+    let mut block_chain = load_system.get_block_chain()?;
+    let mut wallet = load_system.get_wallet()?;
+
+    println!("Wallet: {:?}", wallet);
+
+    get_block_chain(
+        peer_streams,
+        &mut block_chain,
+        connection_config,
+        download_config,
+        logger.clone(),
+    )?;
+
+    // show_merkle_path(&block_chain, logger_sender.clone())?; 
+
+    while account::wants_to_enter_account()? {
+        let new_account = account::add_account(logger.clone())?;
+        wallet.add_account(new_account);
+    }
+
+    let utxo_set = UTXOSet::from_blockchain(&block_chain);
+
+    for account in wallet.accounts.iter() {
+        print!("Account's {} utxo: {:?}\n", account.account_name, utxo_set.get_utxo_list(&Some(account.address.clone())));
+    }
+
+    Ok(SaveSystem::new(
+        block_chain,
+        wallet,
+        logger,
+    ))
+}
