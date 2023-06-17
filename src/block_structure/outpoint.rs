13fd6bfb2ef3c5d683c7b4b8fe433f8f69fc34fd
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-06-16 22:24:19 -0300

    Implemento el hash trait para outpoint

--- src/block_structure/outpoint.rs
+++ src/block_structure/outpoint.rs
@@ -7,7 +7,10 @@ use crate::serialization::{
     serializable_internal_order::SerializableInternalOrder,
     serializable_little_endian::SerializableLittleEndian,
 };
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    hash::{Hash, Hasher},
+};
 
 use std::cmp::PartialEq;
 
@@ -32,5 +35,12 @@ impl DeserializableInternalOrder for Outpoint {
         let index = u32::le_deserialize(stream)?;
 
         Ok(Outpoint { hash, index })
+    }
+}
+
+impl Hash for Outpoint {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.hash.hash(state);
+        self.index.hash(state);
     }
 }
