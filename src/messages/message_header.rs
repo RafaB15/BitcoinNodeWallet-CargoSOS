9077c0971ec6ae32ef3e736b8477dcd1eb96bf0a
sofiajaves <83650724+sofiajaves@users.noreply.github.com>
2023-06-25 19:42:20 -0300

    message header tests + typo

--- src/messages/message_header.rs
+++ src/messages/message_header.rs
@@ -22,7 +22,7 @@ const HEADER_SIZE: usize = MAGIC_BYTES_SIZE + MASSAGE_TYPE_SIZE + PAYLOAD_SIZE +
 pub type MagicType = [u8; 4];
 
 /// It;s the header of any message
-#[derive(Debug)]
+#[derive(Debug, std::cmp::PartialEq)]
 pub struct MessageHeader {
     pub magic_numbers: MagicType,
     pub command_name: CommandName,
@@ -66,6 +66,64 @@ impl DeserializableInternalOrder for MessageHeader {
             payload_size: u32::le_deserialize(stream)?,
             checksum: HashTypeReduced::io_deserialize(stream)?,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test01_correct_message_header_serialization(){
+
+        let mut serialized_fields = vec![];
+        let magic_numbers: [u8; 4] = [0x55, 0x66, 0xee, 0xee];
+        magic_numbers.io_serialize(&mut serialized_fields).unwrap();
+
+        let command_name: CommandName = CommandName::Headers;
+        command_name.io_serialize(&mut serialized_fields).unwrap();
+
+        let payload_size: u32 = 1;
+        payload_size.le_serialize(&mut serialized_fields).unwrap();
+
+        let checksum = [0xC7, 0xF1, 0x8F, 0xE8];
+        checksum.io_serialize(&mut serialized_fields).unwrap();
+
+        let message_header = MessageHeader {
+            magic_numbers,
+            command_name,
+            payload_size,
+            checksum,
+        };
+
+        let mut serialized_message_header = vec![];
+        message_header.io_serialize(&mut serialized_message_header).unwrap();
+
+        assert_eq!(serialized_fields, serialized_message_header);
+        
+    }
+
+    #[test]
+    fn test02_correct_message_header_deserialization(){
+        let magic_bytes: [u8; 4] = [0x55, 0x66, 0xee, 0xee];
+        let checksum = [0xC7, 0xF1, 0x8F, 0xE8];
+
+        let header = MessageHeader {
+            magic_numbers: magic_bytes,
+            command_name: CommandName::Headers,
+            payload_size: 0,
+            checksum,
+        };
+
+        let mut serialized_headers_message = vec![];
+        header.io_serialize(&mut serialized_headers_message).unwrap();
+
+
+        let expected_message_header = MessageHeader::io_deserialize(&mut serialized_headers_message.as_slice()).unwrap();
+
+        assert_eq!(header, expected_message_header);
+
     }
 }
 
