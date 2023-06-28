f1f0e8c05981aa7e6f924df1b63174f32c77ec08
sofiajaves <83650724+sofiajaves@users.noreply.github.com>
2023-06-28 00:41:15 -0300

    modificamos el connection config

--- src/configurations/connection_config.rs
+++ src/configurations/connection_config.rs
@@ -11,10 +11,8 @@ use crate::messages::{bitfield_services::BitfieldServices, message_header::Magic
 
 use std::cmp::PartialEq;
 
-const DNS_SEEDER: &str = "dns_seeder";
 const P2P_PROTOCOL_VERSION: &str = "p2p_protocol_version";
 const IBD_METHOD: &str = "ibd_method";
-const PEER_COUNT_MAX: &str = "peer_count_max";
 const BLOCK_HEIGHT: &str = "block_height";
 const SERVICES: &str = "services";
 const MAGIC_NUMBERS: &str = "magic_numbers";
@@ -25,17 +23,11 @@ const RELAY: &str = "relay";
 /// It represents all the data needed to establish a connection
 #[derive(Debug, PartialEq, Clone)]
 pub struct ConnectionConfig {
-    /// It's the DNS from where the potential peers will be obtaineds
-    pub dns_seeder: DNSSeeder,
-
     /// It's the version of the peer to peer protocol that will be used
     pub p2p_protocol_version: ProtocolVersionP2P,
 
     /// It's the method used for the initial blocks download
     pub ibd_method: IBDMethod,
-
-    /// It's the maximum number of peers that will be connected
-    pub peer_count_max: usize,
 
     /// It's the block height from where the initial blocks download will start
     pub block_height: i32,
@@ -62,10 +54,8 @@ impl Parsable for ConnectionConfig {
         let map = parse_structure(structure)?;
 
         Ok(ConnectionConfig {
-            dns_seeder: DNSSeeder::parse(DNS_SEEDER, &map)?,
             p2p_protocol_version: ProtocolVersionP2P::parse(P2P_PROTOCOL_VERSION, &map)?,
             ibd_method: IBDMethod::parse(IBD_METHOD, &map)?,
-            peer_count_max: usize::parse(PEER_COUNT_MAX, &map)?,
             block_height: i32::parse(BLOCK_HEIGHT, &map)?,
             services: BitfieldServices::parse(SERVICES, &map)?,
             magic_numbers: MagicType::parse(MAGIC_NUMBERS, &map)?,
@@ -85,13 +75,8 @@ mod tests {
     #[test]
     fn test01_accept_valid_input() {
         let configuration = "connection {
-            dns_seeder {
-                seed = seed.testnet.bitcoin.sprovoost.nl
-                port = 18333
-            }
             p2p_protocol_version = V70015
             ibd_method = HeaderFirst
-            peer_count_max = 8
             block_height = 0
             services = [Unname]
             magic_numbers = [1, 2, 3, 4]
@@ -106,10 +91,8 @@ mod tests {
         let connection_result = ConnectionConfig::parse(name, &map);
 
         let config_connection = ConnectionConfig {
-            dns_seeder: DNSSeeder::new("seed.testnet.bitcoin.sprovoost.nl", 18333),
             p2p_protocol_version: ProtocolVersionP2P::V70015,
             ibd_method: IBDMethod::HeaderFirst,
-            peer_count_max: 8,
             block_height: 0,
             services: BitfieldServices {
                 elements: vec![SupportedServices::Unname],
@@ -126,13 +109,8 @@ mod tests {
     #[test]
     fn test02_accepts_input_with_empty_spaces() {
         let configuration = "connection {
-            dns_seeder {
-                seed = seed.testnet.bitcoin.sprovoost.nl
-                             port = 18333
-        }
                       p2p_protocol_version = V70015
             ibd_method=                                 HeaderFirst
-            peer_count_max = 8
             block_height = 0
             services = [Unname]
             magic_numbers = [1, 2, 3, 4]
@@ -147,10 +125,8 @@ mod tests {
         let connection_result = ConnectionConfig::parse(name, &map);
 
         let config_connection = ConnectionConfig {
-            dns_seeder: DNSSeeder::new("seed.testnet.bitcoin.sprovoost.nl", 18333),
             p2p_protocol_version: ProtocolVersionP2P::V70015,
             ibd_method: IBDMethod::HeaderFirst,
-            peer_count_max: 8,
             block_height: 0,
             services: BitfieldServices {
                 elements: vec![SupportedServices::Unname],
@@ -167,12 +143,7 @@ mod tests {
     #[test]
     fn test03_does_not_accept_input_with_missing_values() {
         let configuration = "connection {
-            dns_seeder {
-                seed = seed.testnet.bitcoin.sprovoost.nl
-                port = 18333
-            }
             p2p_protocol_version = V70015
-            peer_count_max = 8
             block_height = 0
             services = [Unname]
             magic_numbers = [1, 2, 3, 4]
@@ -192,14 +163,9 @@ mod tests {
     #[test]
     fn test04_accept_input_with_duplicate_value() {
         let configuration = "connection {
-            dns_seeder {
-                seed = seed.testnet.bitcoin.sprovoost.nl
-                port = 18333
-            }
             p2p_protocol_version = V70015
             ibd_method = HeaderFirst
             ibd_method = HeaderFirst
-            peer_count_max = 8
             block_height = 0
             services = [Unname]
             magic_numbers = [1, 2, 3, 4]
@@ -214,10 +180,8 @@ mod tests {
         let connection_result = ConnectionConfig::parse(name, &map);
 
         let config_connection = ConnectionConfig {
-            dns_seeder: DNSSeeder::new("seed.testnet.bitcoin.sprovoost.nl", 18333),
             p2p_protocol_version: ProtocolVersionP2P::V70015,
             ibd_method: IBDMethod::HeaderFirst,
-            peer_count_max: 8,
             block_height: 0,
             services: BitfieldServices {
                 elements: vec![SupportedServices::Unname],
