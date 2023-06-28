63c7f392d99ea507c45e4761ff5dd4a606a12aae
sofiajaves <83650724+sofiajaves@users.noreply.github.com>
2023-06-28 01:06:27 -0300

    modifico mod

--- src/configurations/mod.rs
+++ src/configurations/mod.rs
@@ -3,6 +3,9 @@ pub mod download_config;
 pub mod log_config;
 pub mod save_config;
 pub mod ui_config;
+pub mod mode_config;
+pub mod server_config;
+pub mod client_config;
 
 pub mod interface;
 pub mod parsable;
